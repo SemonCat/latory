85a38f3287499f1f24ce9f329836b73cf597170e
semon_huang <semon_huang@asus.com>
2017-02-15 13:52:18 +0800

    add more api

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -3,6 +3,7 @@ var express = require('express'),
   Article = require('../models/article');
 
 var gcm = require('node-gcm');
+var sender = new gcm.Sender('AAAAwHckatg:APA91bGSPVowZZcaDU-6ENfwhfa6IlWsY2BTYR1vr66my-FGoc-9hWrDO03EmYtFSoi2Q5PIAMfZF2MJ7usZLKl4i_bVrLamRkb4KAfnoxeF7kIIMrrAxizuRVqgSOsXBk7-knS6x3Uw');
 
 module.exports = function(app) {
   app.use('/', router);
@@ -17,17 +18,42 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/open_app', function(req, res, next) {
-  var pkgName = req.query.pkg_name || "com.android.chrome";
+  var pkgName = req.query.pkg_name || "";
+
+  var intentUri = "";
 
   console.log(pkgName);
-
-  var sender = new gcm.Sender('AAAAwHckatg:APA91bGSPVowZZcaDU-6ENfwhfa6IlWsY2BTYR1vr66my-FGoc-9hWrDO03EmYtFSoi2Q5PIAMfZF2MJ7usZLKl4i_bVrLamRkb4KAfnoxeF7kIIMrrAxizuRVqgSOsXBk7-knS6x3Uw');
 
   // Prepare a message to be sent
   var message = new gcm.Message({
     data: {
       "action": "open_app",
-      "pkg_name": pkgName
+      "pkg_name": pkgName,
+      "uri": intentUri
+    }
+  });
+
+  sender.send(message, {
+    "to": "/topics/open_app"
+  }, function(err, response) {
+    if (err) {
+      console.error(err);
+      res.sendStatus(404);
+      return;
+    }
+
+    console.log(response);
+    res.sendStatus(201);
+  });
+
+});
+
+router.get('/close_app', function(req, res, next) {
+
+  // Prepare a message to be sent
+  var message = new gcm.Message({
+    data: {
+      "action": "close_app"
     }
   });
 
